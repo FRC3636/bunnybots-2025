55b40bb717f5e0f2d87c3d14a268350582f48e9a
Craftzman7 <zezetta@icloud.com>
2025-10-23 07:47:40 -0700

    add motors to CAN.kt

--- src/main/kotlin/com/frcteam3636/bunnybots2025/CAN.kt
+++ src/main/kotlin/com/frcteam3636/bunnybots2025/CAN.kt
@@ -4,6 +4,9 @@ import com.ctre.phoenix6.CANBus
 import com.ctre.phoenix6.hardware.CANcoder
 import com.ctre.phoenix6.hardware.Pigeon2
 import com.ctre.phoenix6.hardware.TalonFX
+import com.revrobotics.spark.SparkFlex
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkMax
 
 private val canivoreBus = CANBus("*")
 
@@ -23,9 +26,26 @@ enum class CTREDeviceId(val num: Int, val bus: CANBus) {
     BackRightTurningEncoder(11, canivoreBus),
     FrontRightTurningEncoder(12, canivoreBus),
 
+    ShooterPivotMotor(13, canivoreBus),
+    IntakePivotMotor(14, canivoreBus),
+    IntakeMotor(15, canivoreBus),
+
     PigeonGyro(20, canivoreBus),
 }
 
 fun CANcoder(id: CTREDeviceId) = CANcoder(id.num, id.bus)
 fun TalonFX(id: CTREDeviceId) = TalonFX(id.num, id.bus)
 fun Pigeon2(id: CTREDeviceId) = Pigeon2(id.num, id.bus)
+
+enum class REVMotorControllerId(val num: Int) {
+    UpperShooterMotor(1),
+    LowerShooterMotor(2),
+    IndexerMotor(3),
+}
+
+
+fun SparkMax(id: REVMotorControllerId, type: SparkLowLevel.MotorType) =
+    SparkMax(id.num, type)
+
+fun SparkFlex(id: REVMotorControllerId, type: SparkLowLevel.MotorType) =
+    SparkFlex(id.num, type)
