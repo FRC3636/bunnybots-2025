dfcd3bd4c3503b053ddbaa9418d3ec43ddcf36e1
Craftzman7 <zezetta@icloud.com>
2025-10-29 15:11:11 -0700

    ok

--- src/main/kotlin/com/frcteam3636/bunnybots2025/subsystems/drivetrain/Drivetrain.kt
+++ src/main/kotlin/com/frcteam3636/bunnybots2025/subsystems/drivetrain/Drivetrain.kt
@@ -13,7 +13,6 @@ import com.frcteam3636.bunnybots2025.subsystems.drivetrain.Drivetrain.Constants.
 import com.frcteam3636.bunnybots2025.subsystems.drivetrain.Drivetrain.Constants.ROTATION_SENSITIVITY
 import com.frcteam3636.bunnybots2025.subsystems.drivetrain.Drivetrain.Constants.TRANSLATION_SENSITIVITY
 import com.frcteam3636.bunnybots2025.utils.fieldRelativeTranslation2d
-import com.frcteam3636.bunnybots2025.utils.getObjectPose
 import com.frcteam3636.bunnybots2025.utils.math.*
 import com.frcteam3636.bunnybots2025.utils.swerve.Corner
 import com.frcteam3636.bunnybots2025.utils.swerve.PerCorner
@@ -227,8 +226,6 @@ object Drivetrain : Subsystem {
             Robot.odometryLock.unlock()
         }
 
-        getObjectPose()
-
         // Update absolute pose sensors and add their measurements to the pose estimator
         for ((name, ioPair) in absolutePoseIOs) {
             val (sensorIO, inputs) = ioPair
@@ -371,10 +368,6 @@ object Drivetrain : Subsystem {
         }.finallyDo { ->
             Logger.recordOutput("Drivetrain/Polar Driving/Active", false)
         }
-    }
-
-    fun alignToCarrot() {
-        val carrotPose = getObjectPose()
     }
 
     private fun calculateInputCurve(input: Double): Double {
