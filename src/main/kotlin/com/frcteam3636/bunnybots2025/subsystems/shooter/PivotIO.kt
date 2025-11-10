7c3e91f191943c69a95553d7c73cb3c156125db5
Craftzman7 <zezetta@icloud.com>
2025-11-10 15:25:44 -0800

    make intellij happy

--- src/main/kotlin/com/frcteam3636/bunnybots2025/subsystems/shooter/PivotIO.kt
+++ src/main/kotlin/com/frcteam3636/bunnybots2025/subsystems/shooter/PivotIO.kt
@@ -85,7 +85,7 @@ class PivotIOReal : PivotIO {
         }
     }
 
-    val positionControl = MotionMagicVoltage(0.0).withUpdateFreqHz(0.0)
+    val positionControl: MotionMagicVoltage = MotionMagicVoltage(0.0).withUpdateFreqHz(0.0)
 
     override fun turnToAngle(angle: Angle) {
         assert(angle > 0.degrees)
